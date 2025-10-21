02f20ce81031099f8a01709fd7167577cfc6db99
biswajitrout45 <routbiswajit475@gmail.com>
2025-10-21 19:33:45 +0530

    Reverse a string

---
+++ stringrev.js
@@ -0,0 +1,7 @@
+let a="hiiiiiii"
+let b=''
+for(i=0;i<a.length;i++)
+{
+    b=a[i]+b
+}
+console.log(b)
---
+++ stringrevfunc.js
@@ -0,0 +1,4 @@
+function rev(a){
+    return a.split("").reverse().join("")
+}
+console.log(rev("Biswajit Rout"))
