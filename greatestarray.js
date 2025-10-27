452daacac6ffe40af219d25294f21d30207c0d44
biswajitrout45 <routbiswajit475@gmail.com>
2025-10-27 09:18:13 +0530

    greatest from the array element

--- greatestarray.js
+++ greatestarray.js
@@ -9,17 +9,3 @@ for(i=0;i<arr.length;i++)
 }
 console.log(temp)
 
-/*let arr=[10,20,4,556,4891,1,12,46]
-    function larg(arr)
-    {
-        let temp=arr[0]
-        for(i=0;i<arr.length;i++)
-        {
-            if(temp<=arr[i])
-            {
-                temp=arr[i]
-            }
-        }    
-        return temp
-    }
-        console.log(temp)*/
